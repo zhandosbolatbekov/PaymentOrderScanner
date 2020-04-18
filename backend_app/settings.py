d0b0d4e603b53ca6fe1be1a68ae1472c352402cb
Zhandos Bolatbekov <bolatbekov@strongte.am>
2020-04-18 23:36:18 +0600

    some fixes

--- backend_app/settings.py
+++ backend_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y%d$45r%8%tb$b^0_g_@%31qgwit35(rw&hfvnukfjf*se=@df'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['herokuscandocapp.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'herokuscandocapp.herokuapp.com']
 
 # Application definition
 
@@ -129,5 +129,5 @@ STATICFILES_DIRS = (
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-prod_db = dj_database_url.config(conn_max_age=500)
+prod_db = dj_database_url.config()
 DATABASES['default'].update(prod_db)
